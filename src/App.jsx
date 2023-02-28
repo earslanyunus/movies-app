e4f3dc2b51a44b79e56970b1082f172bd9c4070c
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-02-28 19:04:57 +0300

    updated font classes and clear unused codes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Text from "./components/input/Text.jsx";
+import Button from "./components/button/Button.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Text/>
+        <Button type={'link'}/>
     </div>
   )
 }
--- src/components/input/Text.jsx
+++ src/components/input/Text.jsx
@@ -5,8 +5,8 @@ function Text({label="Label",placeholder="Test",id}) {
     return (
 
         <div className={'flex flex-col gap-1.w-full'}>
-            <label className='font-medium text-uui-text-sm leading-uui-text-sm text-gray-700' htmlFor={id}>{label}</label>
-            <input type="text" name="" id={id} placeholder={placeholder} className='w-full rounded text-gray-900 py-2.5 px-3.5 rounded-lg border border-gray-300  placeholder:text-gray-500 focus:border-primary-300  focus:shadow-[0px_0px_0px_4px_#F4EBFF]'/>
+            <label className='font-medium text-text-sm  text-gray-700' htmlFor={id}>{label}</label>
+            <input  name="" id={id} placeholder={placeholder} className='w-full rounded text-gray-900 py-2.5 px-3.5 rounded-lg border border-gray-300  placeholder:text-gray-500 focus:border-primary-300  focus:shadow-[0px_0px_0px_4px_#F4EBFF]'/>
         </div>
 
     );
