cb19551c05e3a2a3caebee6134c6cf9ba40f3e9b
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-02-28 14:51:29 +0300

    finished text input

--- src/components/input/Text.jsx
+++ src/components/input/Text.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function Text(props) {
+function Text({label="Label",placeholder="Test",id}) {
+
     return (
-        <input type="text"/>
+
+        <div className={'flex flex-col gap-1.w-full'}>
+            <label className='font-medium text-uui-text-sm leading-uui-text-sm text-gray-700' htmlFor={id}>{label}</label>
+            <input type="text" name="" id={id} placeholder={placeholder} className='w-full rounded text-gray-900 py-2.5 px-3.5 rounded-lg border border-gray-300  placeholder:text-gray-500 focus:border-primary-300  focus:shadow-[0px_0px_0px_4px_#F4EBFF]'/>
+        </div>
+
     );
 }
 
