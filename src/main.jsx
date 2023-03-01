35e52ac7b136ddf6af2c363a4fc21731e9f54045
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-01 20:26:15 +0300

    installed react router and setup complete for signup page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Signup from "./views/Signup.jsx";
+const router = createBrowserRouter([{
+    path: '/',
+    element: <App/>,
+    children:[
+        {
+            path: '/signup',
+            element: <Signup/>,
+        }
+    ]
+}])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>,
 )
