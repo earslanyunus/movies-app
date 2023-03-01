242a878138b503fb94bda40f83b856b4d3598cc3
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-01 20:30:07 +0300

    New props were added and className was made usable for control purposes.

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import googleIcon from '../../assets/googleIcon.svg'
 
-function Button({text="Button",wFull=false,type}) {
+function Button({text="Button",wFull=false,variant,extraClass='',isSocial=false,...props}) {
     const initialClass = `
-    ${(type=== 'primary'||type === 'secondary') ? 'py-2.5 px-[1.125rem] rounded-lg text-text-md ':''}
-    ${type=== 'primary'? 'bg-primary-600  text-white ':''}
-    ${type=== 'link'? 'text-primary-700  font-semibold text-sm' : ' ' } 
+    ${(variant=== 'primary'||variant === 'secondary') ? 'py-2.5 px-[1.125rem] rounded-lg text-text-md font-semibold':''}
+    ${variant=== 'primary'? 'bg-primary-600  text-white ':''}
+    ${variant=== 'secondary'? 'bg-white border border-gray-300 text-gray-700 ':''}
+    ${variant=== 'link'? 'text-primary-700  font-semibold text-sm' : ' ' } 
+    ${isSocial? 'flex items-center justify-center gap-3' : ''}
+    
     ${wFull? 'w-full' : '' }
     `
     return (
         <>
-        <button type={"button"} className={initialClass}>{text}</button>
-            <p className='text-x'>Deneme Yazisi</p>
+        <button  {...props}  className={initialClass+extraClass}>{isSocial&&<img src={googleIcon} alt={'google icon '}/>}{text}</button>
+
         </>
     );
 }
