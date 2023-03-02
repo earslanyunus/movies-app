8aacc8ff70cce1707407c7236aa69922327feb50
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-03 00:47:30 +0300

    added movies app logo and footer information

--- src/views/Signup.jsx
+++ src/views/Signup.jsx
@@ -5,6 +5,8 @@ import {Formik, Form, ErrorMessage} from "formik";
 import Button from "../components/button/Button.jsx";
 import * as Yup from 'yup';
 import File from "../components/input/File.jsx";
+import fullLogo from "../assets/LogoandLogotype.svg";
+import mailIcon from "../assets/mailIcon.svg";
 
 const signUpSchema = Yup.object().shape({
     username: Yup.string().min(2, 'Too Short!').max(12, 'Too Long!').required('Required'),
@@ -17,11 +19,12 @@ function Signup() {
     return (
         // 20 px to rem =
         <div className='px-4 pt-14 pb-8 flex w-full lg:p-0 lg:h-screen lg:max-h-screen lg:min-h-[768px]'>
-            <div className='flex items-center justify-center w-full   lg:w-1/2'>
+            <div className='flex flex-col items-center justify-between w-full   lg:w-1/2 lg:px-4 lg:pt-14 lg:pb-8'>
+                <img className='hidden lg:block' src={fullLogo} alt="logo"/>
                 <div className='w-full h-full flex flex-col items-center justify-center lg:w-1/2'>
-                    <img className='h-[32px] w-[32px] 2xl:h-[64px] 2xl:w-[64px]' src={logo} alt=""/>
-                    <p className='text-display-xs font-semibold mt-6 text-center'>Create an account</p>
-                    <p className='text-text-md font-normal mt-2 text-center'>Watching alone is enough no more :)</p>
+                    <img className='h-[32px] w-[32px] 2xl:h-[64px] 2xl:w-[64px] lg:hidden' src={logo} alt=""/>
+                    <p className='text-display-xs font-semibold mt-6 lg:self-start'>Create an account</p>
+                    <p className='text-text-md font-normal mt-2 lg:self-start'>Watching alone is enough no more :)</p>
                     <Formik
                         initialValues={{
                             username: '',
@@ -74,9 +77,16 @@ function Signup() {
                     </div>
 
                 </div>
+                <div className='hidden lg:flex justify-between items-center w-full'>
+                    <p className='font-normal text-text-sm text-gray-600'>© Movies App</p>
+                    <div className='flex gap-2'>
+                        <img src={mailIcon} alt="mailicon"/>
+                        <p className='font-normal text-text-sm text-gray-600'>yyunus.earslan@gmail.com</p>
+
+                    </div>
+                </div>
             </div>
             <div className='hidden lg:block lg:w-1/2 bg-signUp bg-cover'>
-                {/*<img classNameame='h-full' src="https://www.themoviedb.org/t/p/original/8gVy5MLXtZBWghMykQtPMsNc5kH.jpg" alt=""/>*/}
             </div>
         </div>
 
