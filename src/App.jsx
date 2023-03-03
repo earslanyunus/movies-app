4060410b5a4a1bde731e2871ff19993ad663da8b
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-03 19:39:50 +0300

    added navigate for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 function App() {
+    //page open and navigate to home
+    const navigate = useNavigate();
+    useEffect(() => {
+        navigate('/home')
+    }, [])
+
+
 
   return (
     <>
