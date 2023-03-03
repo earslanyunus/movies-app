645627e87373740078e6616c95afab2cb8da3ef6
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-03 19:41:02 +0300

    added store and private router for signup page

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,21 @@ import App from './App'
 import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Signup from "./views/Signup.jsx";
+import Store from "./store/index.js";
+import {Provider} from "react-redux";
+import PrivateRoute from "./router/PrivateRoute.jsx";
 const router = createBrowserRouter([{
     path: '/',
     element: <App/>,
     children:[
         {
             path: '/signup',
-            element: <Signup/>,
+            element: <PrivateRoute><Signup/></PrivateRoute>,
+        },
+        {
+            path: '/home',
+            element: <div>Home</div>,
+
         }
     ]
 }])
@@ -18,6 +26,11 @@ const router = createBrowserRouter([{
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+
+    <Provider store={Store}  >
       <RouterProvider router={router}/>
+    </Provider>
+
   </React.StrictMode>,
 )
