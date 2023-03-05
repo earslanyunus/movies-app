ea03cd4b6101cf974bdaf9210f14d5ca8de78143
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-05 12:32:59 +0300

    updated firebase environment

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -14,6 +14,7 @@ import {doc, getFirestore, setDoc} from "firebase/firestore";
 import store from "../store/index.js";
 import {useNavigate} from "react-router-dom";
 
+console.log(process.env)
 
 const firebaseConfig={
     apiKey:process.env.VITE_FIREBASE_API_KEY,
