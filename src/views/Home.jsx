f97e001b4afc5591a4138e04560e711e646dba2b
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-05 13:28:28 +0300

    updated .env

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,6 +4,7 @@ import {signOut} from "../firebase/index.js";
 function Home(props) {
     return (
         <div>
+            {import.meta.env.VITE_TEST_KEY}
             <button onClick={signOut}>Sign out</button>
         </div>
     );
