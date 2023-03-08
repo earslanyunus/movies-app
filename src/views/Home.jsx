c44d8a519448ab30cac3c17269f5315184f0147a
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-08 15:25:37 +0300

    PerView component added new props and the p tags were updated.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -20,7 +20,8 @@ function Home(props) {
         <div>
 
             <div className='h-[30vh]'>
-            <PerView items={popularMovies}/>
+                <p className='container mx-auto text-display-md  font-semibold text-gray-900 mb-3'>Popular Movies</p>
+            <PerView slidesPerView={'auto'} spaceBetween={12}  items={popularMovies} arrows={true}/>
             </div>
         </div>
     );
