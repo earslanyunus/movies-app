08ec248a68a15a2a991d133c170271827caadba6
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-08 15:24:33 +0300

    getMovieCredits and getMovieDetail functions were created.

--- src/utils/MovieDb.js
+++ src/utils/MovieDb.js
@@ -9,5 +9,19 @@ const getMovie = async (id) => {
     const response = await fetch(url);
     return await response.json();
 }
+const getMovieCredits = async (id) => {
+    const url = `${API_URL}/movie/${id}/credits?api_key=${import.meta.env.VITE_MOVIEDB_API_KEY}&language=en-US`;
+    const response = await fetch(url);
+    return await response.json();
+}
+//combine detail and credits
+const getMovieDetail = async (id) => {
+    const movie = await getMovie(id);
+    const credits = await getMovieCredits(id);
+    return {
+        ...movie,
+        ...credits
+    }
+}
 
-export { getPopularMovies,getMovie }
+export { getPopularMovies,getMovie,getMovieDetail }
