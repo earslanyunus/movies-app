21b7d71a19db7686f8a515275aa9a01baf3f09a0
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2023-03-19 03:05:56 +0300

    New movies section was added to the homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,23 @@
 import React, {useEffect, useState} from 'react';
-import {getMovie, getPopularMovies} from "../utils/MovieDb.js";
+import {getPopularMovies, getTopRatedMovies, getUpcomingMovies} from "../utils/MovieDb.js";
 import PerView from "../components/swiper/PerView.jsx";
 
 function Home(props) {
     const [popularMovies, setPopularMovies] = useState([]);
+    const [topRatedMovies, setTopRatedMovies] = useState([]);
+    const [upcomingMovies, setUpcomingMovies] = useState([]);
     useEffect(() => {
         getPopularMovies().then((data) => {
-            setPopularMovies(data.results);
+                setPopularMovies(data.results);
+            }
+        )
+
+        getTopRatedMovies().then((data) => {
+            setTopRatedMovies(data.results);
+        })
+
+        getUpcomingMovies().then((data) => {
+            setUpcomingMovies(data.results);
         }
         )
 
@@ -16,10 +27,22 @@ function Home(props) {
     return (
         <div>
 
-            <div className='h-[30vh]'>
+
+            <div className='h-[40vh] flex flex-col min-h-[25rem]'>
                 <p className='container mx-auto text-display-md  font-semibold text-gray-900 mb-3'>Popular Movies</p>
-            <PerView slidesPerView={'auto'} spaceBetween={12}  items={popularMovies} arrows={true}/>
+                <PerView slidesPerView={'auto'} spaceBetween={12} items={popularMovies} arrows={true}/>
             </div>
+            <div className='h-[40vh] mt-6 flex flex-col'>
+                <p className='container mx-auto text-display-md  font-semibold text-gray-900 mb-3'>Top Rated Movies</p>
+                <PerView slidesPerView={'auto'} spaceBetween={12} items={topRatedMovies} arrows={true}/>
+
+            </div>
+            <div className='h-[40vh] mt-6 flex flex-col'>
+                <p className='container mx-auto text-display-md  font-semibold text-gray-900 mb-3'>Upcoming Movies</p>
+                <PerView slidesPerView={'auto'} spaceBetween={12} items={upcomingMovies} arrows={true}/>
+
+            </div>
+
         </div>
     );
 }
